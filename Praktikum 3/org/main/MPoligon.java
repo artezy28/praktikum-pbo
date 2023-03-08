c9bfe107f422a4615dc29f106950fb6bb74ce340
28ilham <96777563+28ilham@users.noreply.github.com>
2023-03-08 17:23:19 +0700

    Add files via upload

---
+++ Praktikum 3/org/main/MPoligon.java
@@ -0,0 +1,22 @@
+/**
+* File : MPoligon.java 8/Maret/2023
+* Penulis : Majid Ilham Adhim
+* Deskripsi : driver class untuk poligon dan persegi panjang dan segitiga
+**/
+
+package org.main;
+
+import org.bangundatar.*;
+
+public class MPoligon{
+	public static void main(String[] args){
+		PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+		persegi.printInfo();
+		System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
+		
+		Segitiga segitiga = new Segitiga(5,10,3);
+		segitiga.printInfo();
+		System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
+		
+	}
+}
